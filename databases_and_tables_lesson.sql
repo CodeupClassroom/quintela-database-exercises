ed088a7d89ce36e90fd901b2ef6b936b005a682f
Ryan McCall <ryan.e.mccall@gmail.com>
2023-05-01 14:40:59 -0500

    Add databases and tables lesson notes

---
+++ databases_and_tables_lesson.sql
@@ -0,0 +1,29 @@
+-- Here is a comment
+
+/*
+Here is 
+a multiline
+comment
+*/
+
+SHOW databases;
+
+USE albums_db;
+
+SELECT database();
+
+SHOW CREATE DATABASE albums_db;
+
+SHOW databases;
+
+USE farmers_market;
+
+SELECT database();
+
+SHOW tables;
+
+SHOW CREATE TABLE booth;
+
+SHOW tables;
+
+SHOW CREATE TABLE customer;
